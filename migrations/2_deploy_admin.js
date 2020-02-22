75ae02d08c841b8be88518bbcfcf412f253741bc
anon <sarah.anne.friend@gmail.com>
2020-02-21 20:04:56 -0500

    Works with unset env vars

--- migrations/2_deploy_admin.js
+++ migrations/2_deploy_admin.js
@@ -1,5 +1,7 @@
 const Admin = artifacts.require('Admin');
 
-module.exports = function (deployer) {
-  deployer.deploy(Admin, [process.env.ADDR1, process.env.ADDR2]);
+module.exports = function (deployer, network, accounts) {
+  const address1 = process.env.ADDR1 || accounts[0];
+  const address2 = process.env.ADDR2 || accounts[1];
+  deployer.deploy(Admin, [address1, address2]);
 };
