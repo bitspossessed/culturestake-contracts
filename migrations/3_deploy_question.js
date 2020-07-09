d7c9ea3e4a48c83346996e8fe378be3042275a81
anon <sarah.anne.friend@gmail.com>
2020-07-09 10:47:15 -0400

    Correct endtime

--- migrations/3_deploy_question.js
+++ migrations/3_deploy_question.js
@@ -1,10 +1,11 @@
 const Culturestake = artifacts.require('Culturestake');
+const { timestamp } = require('../test/helpers/constants');
 
 module.exports = function (deployer) {
   const festival = web3.utils.sha3('festival');
   Culturestake.deployed()
     .then(async (culturestake) => {
-      await culturestake.initFestival(festival, Math.floor((new Date()).getTime() / 1000), 10000);
+      await culturestake.initFestival(festival, timestamp(), timestamp() + 10000);
       await culturestake.initQuestion(100, festival);
       await culturestake.initVotingBooth(festival, process.env.BOOTH);
     });
