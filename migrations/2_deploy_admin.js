3f759fd04a235a1548dd67dbab4b93552dade857
anon <sarah.anne.friend@gmail.com>
2020-07-18 14:41:12 -0400

    Use account 0 by default

--- migrations/2_deploy_admin.js
+++ migrations/2_deploy_admin.js
@@ -1,8 +1,9 @@
 const Culturestake = artifacts.require('Culturestake');
 
 module.exports = function (deployer, network, accounts) {
-  const address1 = process.env.GANACHE_ACCOUNT_0 || accounts[0];
-  const address2 = process.env.METAMASK || accounts[1];
+  const address0 = accounts[0];
+  const address1 = process.env.GANACHE_ACCOUNT || accounts[1];
+  const address2 = process.env.METAMASK || accounts[2];
   deployer.deploy(Culturestake, [address1, address2]);
 };
 
