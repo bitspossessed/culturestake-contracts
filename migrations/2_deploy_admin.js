5174e4fc3e62b5aadb9f7a8c282afef94f01d813
anon <sarah.anne.friend@gmail.com>
2020-07-18 14:39:34 -0400

    Rename env vars

--- migrations/2_deploy_admin.js
+++ migrations/2_deploy_admin.js
@@ -1,8 +1,8 @@
 const Culturestake = artifacts.require('Culturestake');
 
 module.exports = function (deployer, network, accounts) {
-  const address1 = process.env.ADDR1 || accounts[0];
-  const address2 = process.env.ADDR2 || accounts[1];
+  const address1 = process.env.GANACHE_ACCOUNT_0 || accounts[0];
+  const address2 = process.env.METAMASK || accounts[1];
   deployer.deploy(Culturestake, [address1, address2]);
 };
 
